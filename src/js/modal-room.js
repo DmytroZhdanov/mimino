35a300ddb36c7d5b1ddbbbc6a21b767bb37319ae
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-02-27 14:54:57 +0000

    Ammends modals js

--- src/js/modal-room.js
+++ src/js/modal-room.js
@@ -1,13 +1,14 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-room-open-a]'),
-    openModalBtn: document.querySelector('[data-modal-room-open-b]'),
+    openSecondModalBtn: document.querySelector('[data-modal-room-open-b]'),
     closeModalBtn: document.querySelector('[data-modal-room-close]'),
     modal: document.querySelector('[data-modal-room]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openSecondModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
--- src/js/modal-table.js
+++ src/js/modal-table.js
@@ -1,13 +1,14 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-table-open-a]'),
-    openModalBtn: document.querySelector('[data-modal-table-open-b]'),
+    openSecondModalBtn: document.querySelector('[data-modal-table-open-b]'),
     closeModalBtn: document.querySelector('[data-modal-table-close]'),
     modal: document.querySelector('[data-modal-table]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openSecondModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
